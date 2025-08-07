bc5483dc3b21ab3e58f8f5a34ff25ce0677ea9f7
Scarllette Ellis <re412@bath.ac.uk>
2025-08-07 11:40:41 +0100

    SAC example in main

--- main.py
+++ main.py
@@ -10,51 +10,6 @@ from utils.load_agent import load_agent
 
 if __name__ == '__main__':
 
-    environment = gym.make('CartPole-v1')
-
-    dqn_agent = DQN(
-        environment,
-        [128, 84],
-        10000
-    )
-
-    print("Training Agent")
-    train_agent(
-        environment,
-        dqn_agent,
-        50_000,
-        0,
-        0,
-        progress_bar=True
-    )
-
-    environment = gym.make("CartPole-v1")
-    episode_over = False
-    state, _ = environment.reset()
-    total_reward_pre_load = 0
-    while not episode_over:
-        action = dqn_agent.choose_action(state)  # agent policy that uses the observation and info
-        state, reward, terminated, truncated, _ = environment.step(action)
-        episode_over = terminated or truncated
-        total_reward_pre_load += reward
-
-    dqn_agent.save(Path('trained-agents/dqn_test'))
-    new_agent = load_agent(environment, Path('trained-agents/dqn_test'), DQN)
-
-    environment = gym.make("CartPole-v1")
-    episode_over = False
-    state, _ = environment.reset()
-    total_reward_post_load = 0
-    while not episode_over:
-        action = new_agent.choose_action(state)  # agent policy that uses the observation and info
-        state, reward, terminated, truncated, _ = environment.step(action)
-        episode_over = terminated or truncated
-        total_reward_post_load += reward
-
-    print("Reward pre load: " + str(total_reward_pre_load))
-    print("Reward post load: " + str(total_reward_post_load))
-    exit()
-
     environment = gym.make("CartPole-v1")
 
     print("Creating SAC Agent")
