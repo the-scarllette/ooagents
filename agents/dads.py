de80d379ebc335e1ceb0ed634023aae73cae198f
callum-pitceathly <callumpitceathly@gmail.com>
2025-08-07 20:36:53 +0100

    Removed models folder for now

--- agents/dads.py
+++ agents/dads.py
@@ -6,8 +6,34 @@ import optax
 import jax
 import flax.linen as nn
 from jax import random, numpy as jnp
-from Models.models import GaussianModule, DiscreteModule
 from flax.training.train_state import TrainState
+
+class GaussianModule(nn.Module):
+    fix_std: bool
+    hidden_features: List[int]
+    output_features: int
+    
+    @nn.compact
+    def __call__(self, x: jnp.ndarray) -> jnp.ndarray:
+        for features in self.hidden_features:
+            x = nn.Dense(features)(x)
+            x = nn.relu(x)
+
+        mu = nn.Dense(self.output_features)(x)
+        log_sigma = nn.Dense(self.output_features)(x) if not self.fix_std else jnp.zeros((mu.shape[0], self.output_features))
+        return mu, log_sigma
+    
+class DiscreteModule(nn.Module):
+    hidden_features: List[int]
+    
+    @nn.compact
+    def __call__(self, x:jnp.ndarray) -> jnp.ndarray:
+        for features in self.hidden_features:
+            x = nn.Dense(features)(x)
+            x = nn.relu(x)
+            
+        x = nn.Dense(x)
+        return x
 
 class SkillDynamicsModel(nn.Module):
     """
